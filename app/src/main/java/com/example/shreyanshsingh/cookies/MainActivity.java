4a95cf0eceb1643a24598323bad6f7892d3c980a
Shreyansh <shreyansh.pettswood@gmail.com>
2016-12-16 13:01:04 +0530

    First commit

---
+++ app/src/main/java/com/example/shreyanshsingh/cookies/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.shreyanshsingh.cookies;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+
+        public void eatCookie (View view){
+            // TODO: Find a reference to the ImageView in the layout. Change the image.
+            ImageView image = (ImageView) findViewById(R.id.android_cookie_image_view);
+            image.setImageResource(R.drawable.after_cookie);
+            // TODO: Find a reference to the TextView in the layout. Change the text.
+            TextView text = (TextView) findViewById(R.id.status_text_view);
+            text.setText("I'm so full");
+            text.setTextColor(Color.WHITE);
+            text.setTextSize(34);
+
+            view.setVisibility(View.GONE);
+        }
+
+}
